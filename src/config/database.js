b730ed03fa524301ffc596a69d4c6135ac6d87ca
Pulido V. P. E <pulidovpe.dev@gmail.com>
2018-07-26 12:23:22 -0400

    Deploy to heroku. Se agrego mLab y modifico package.json para no usar nodemon

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose');
 const { db: { host, port, name } } = require('./config');
 
-const connStr = `mongodb://${host}:${port}/${name}`;
+/*const connStr = `mongodb://${host}:${port}/${name}`;*/
+const connStr = `mongodb://pulidovpe:esfinge.677@ds255451.mlab.com:55451/mean-library`;
 
 mongoose.connect(connStr)
   .then( () => console.log(`DB ${name} is connected`) )
