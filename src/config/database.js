e1f3bc78fed588e8433386a6eabc168f1076b85c
Pulido V. P. E <pulidovpe.dev@gmail.com>
2018-07-28 20:50:40 -0400

    Implementacion base de datos mLab, tambien de ngx-pagination. Se agrego paginacion en las tablas de los 3 componentes y un pipe-filtro para los libros

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { db: { host, port, name } } = require('./config');
 
 /*const connStr = `mongodb://${host}:${port}/${name}`;*/
-const connStr = `mongodb://pulidovpe:esfinge.677@ds255451.mlab.com:55451/mean-library`;
+const connStr = `mongodb://**********:**********@ds255451.mlab.com:55451/mean-library`;
 
 mongoose.connect(connStr)
   .then( () => console.log(`DB ${name} is connected`) )
